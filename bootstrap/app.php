ae0c2b837768d6744e38edfc79cb550b5a60619e
MacBook Pro <billiegate8@gmail.com>
2023-05-15 12:10:36 +0100

    updated routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -116,4 +116,10 @@ $app->router->group([
     require __DIR__.'/../routes/web.php';
 });
 
+$app->router->group([
+    'namespace' => 'App\Http\Controllers',
+], function ($router) {
+    require __DIR__.'/../routes/web.php';
+});
+
 return $app;
